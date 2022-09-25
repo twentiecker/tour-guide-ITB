7e13c480f4ab2f03d3e066db05d0f21ccadd5d79
twentiecker <twentiecker@gmail.com>
2022-09-25 21:14:51 +0700

    retrofit liststory

--- app/src/main/java/com/twentiecker/storyapp/liststory/ListStoryActivity.kt
+++ app/src/main/java/com/twentiecker/storyapp/liststory/ListStoryActivity.kt
@@ -2,10 +2,43 @@ package com.twentiecker.storyapp.liststory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.twentiecker.storyapp.R
+import com.twentiecker.storyapp.authentication.register.RegisterConfig
+import com.twentiecker.storyapp.authentication.register.RegisterResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListStoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_story)
+
+        val service = ListStoryConfig.getListStoryService().getListStory(1)
+        service.enqueue(object : Callback<ListStoryResponse> {
+            override fun onResponse(
+                call: Call<ListStoryResponse>,
+                response: Response<ListStoryResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null && !responseBody.error) {
+                        Toast.makeText(this@ListStoryActivity, responseBody.message, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                } else {
+                    Toast.makeText(this@ListStoryActivity, response.message(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<ListStoryResponse>, t: Throwable) {
+                Toast.makeText(
+                    this@ListStoryActivity,
+                    "Id tidak ditemukan",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/twentiecker/storyapp/liststory/ListStoryService.kt
+++ app/src/main/java/com/twentiecker/storyapp/liststory/ListStoryService.kt
@@ -1,4 +1,13 @@
 package com.twentiecker.storyapp.liststory
 
-class ListStoryService {
+import com.twentiecker.storyapp.authentication.register.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ListStoryService {
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLUQ2bkZoNlE4MlBiT21FRk0iLCJpYXQiOjE2NjM5MDc2NTF9.7m436ENApR4-72l5-VeRd-WHxR3YagvHiuGhyFVNc8s")
+    @GET("stories")
+    fun getListStory(
+        @Query("page") page: Int
+    ): Call<ListStoryResponse>
 }
