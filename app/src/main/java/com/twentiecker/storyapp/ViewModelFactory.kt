126bfdd02a49fb6e7a2af5486e9963f316290b63
twentiecker <twentiecker@gmail.com>
2022-10-03 22:26:09 +0700

    add viewmodelfactory

---
+++ app/src/main/java/com/twentiecker/storyapp/ViewModelFactory.kt
@@ -0,0 +1,27 @@
+package com.twentiecker.storyapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.twentiecker.storyapp.authentication.login.LoginViewModel
+import com.twentiecker.storyapp.authentication.register.SignupViewModel
+import com.twentiecker.storyapp.liststory.MainViewModel
+import com.twentiecker.storyapp.model.UserPreference
+
+class ViewModelFactory(private val pref: UserPreference) : ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel(pref) as T
+            }
+            modelClass.isAssignableFrom(SignupViewModel::class.java) -> {
+                SignupViewModel(pref) as T
+            }
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(pref) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+}
