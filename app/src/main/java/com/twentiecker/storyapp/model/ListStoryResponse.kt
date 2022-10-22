50d8c7d8308b0f7965b2f9d2bfece49f26ce8d1f
twentiecker <twentiecker@gmail.com>
2022-10-23 06:51:15 +0700

    fix parcelize

--- app/src/main/java/com/twentiecker/storyapp/model/ListStoryResponse.kt
+++ app/src/main/java/com/twentiecker/storyapp/model/ListStoryResponse.kt
@@ -2,7 +2,7 @@ package com.twentiecker.storyapp.model
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 data class ListStoryResponse(
 	@field:SerializedName("listStory")
